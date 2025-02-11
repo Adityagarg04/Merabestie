bacfc015beddb7312ff78d4005f7eba910a0df67
Aditya <adityagarg485@gmail.com>
2025-02-11 21:18:03 +0530

    commit 1

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 function Homepage() {
   return (
     <div
-      className="container bg-cover bg-center bg-no-repeat h-screen w-full flex justify-center items-center"
-      style={{ backgroundImage: "url('./src/image/homebackground.jpg')" }}
+      className="bg-cover bg-center bg-no-repeat h-screen w-full flex justify-center items-center"
+      style={{
+        backgroundImage: "url('./src/image/homebackground.jpg')",
+        backgroundSize: 'cover',  // Ensure the image always covers the full screen
+      }}
     >
       <h1 className="text-center p-5 text-white bg-black bg-opacity-50 text-4xl font-bold rounded-[40px]">
         Personalized Stationery
