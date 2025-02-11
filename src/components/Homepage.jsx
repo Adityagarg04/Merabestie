7a5186845595164a920de1cab7b64c46216120a2
saksham <sakshamkhandelwal208@gmail.com>
2025-02-11 21:16:01 +0530

    commit 4

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import backgroundImage from '../image/homebackground.jpg'; // Adjust the path based on location
 
 function Homepage() {
   return (
     <div
-      className="container bg-cover bg-center bg-no-repeat h-screen w-full flex justify-center items-center"
-      style={{ backgroundImage: "url('./src/image/homebackground.jpg')" }}
+      className=" bg-cover bg-center bg-no-repeat   flex justify-center items-center"
+      style={{ backgroundImage: `url(${backgroundImage})`,
+      width: "100vw",  /* Full width of the viewport */
+      height: "100vh" }}
     >
       <h1 className="text-center p-5 text-white bg-black bg-opacity-50 text-4xl font-bold rounded-[40px]">
         Personalized Stationery
