439e5c2fabc8b7990a3499cefe0d6c883a30c187
saksham <sakshamkhandelwal208@gmail.com>
2025-02-11 19:28:25 +0530

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Homepage from './components/Homepage'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar/>
   <Homepage/>
+  
+
+
+
     </>
   )
 }
